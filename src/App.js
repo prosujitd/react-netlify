f63a86182aa967bd54539e2de471265266de1111
Sujit Dahal <prosujit@gmail.com>
2021-07-10 19:17:51 +0545

    deploy changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Netlify - Automatic Deploy
+        Netlify - Automatic Deployment
       </header>
     </div>
   );
