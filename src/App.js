edb88db21638b0f31e76e19fe899d1dcfc103a5b
Sujit Dahal <prosujit@gmail.com>
2021-07-10 19:33:36 +0545

    ha ha

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        Removed...
-      </header>
+      Final Removed ho hai
     </div>
   );
 }
